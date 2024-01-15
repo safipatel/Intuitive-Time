d4a15214c05e5ae988f254fbe11158189d65ed1b
Safi Patel <safipatel1@gmail.com>
2024-01-15 01:49:58 -0500

    improved responsive styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import React, {
-  MutableRefObject,
-  RefObject,
   memo,
   useCallback,
   useEffect,
@@ -42,8 +40,6 @@ const firebaseApp = initializeApp({
 });
 
 const db = getFirestore(firebaseApp);
-
-const GaugeMemo = memo(Gauge);
 
 type ReturnStruct = {
   start: Timestamp;
@@ -108,8 +104,6 @@ function AddButton() {
   );
 }
 
-
-
 function DisplayStats() {
   // Load-in data from firebase
   const q = useMemo(() => {
@@ -121,24 +115,25 @@ function DisplayStats() {
   // console.log(loadedStartTimeContainer?.[0].start);
   // console.log(status);
   // const startTime = moment(loadedStartTimeContainer?.[0].start.toDate());
-  const graphRef = useRef<Plot<any> | null>(null);
 
   // States that update every second or if startTime changes
   const [startTime, setStartTime] = useState<Moment>(moment());
+
+  // Ref that refers to the gauge
+  const gaugeRef = useRef<Plot<any> | null>(null);
 
   // Callback function that updates everytime the data changes
   const updateStats = useCallback(() => {
     if (!loadedStartTimeContainer?.[0]?.start) {
       return;
     }
-    setStartTime(moment(loadedStartTimeContainer?.[0].start.toDate()));
-    // setTimeSpentDuration(
-    //   moment.duration(moment().diff(loadedStartTimeContainer[0].start.toDate()))
-    // );
-    if(graphRef.current){
-      const st = (moment.duration(moment().diff(moment(loadedStartTimeContainer?.[0].start.toDate()))).asSeconds() / (16 * 60 * 60))
+    const st = moment(loadedStartTimeContainer?.[0].start.toDate());
+    setStartTime(st);
 
-      graphRef.current.changeData(st);
+    if (gaugeRef.current) {
+      const percSpent =
+        moment.duration(moment().diff(st)).asSeconds() / (16 * 60 * 60);
+      gaugeRef.current.changeData(percSpent);
     }
   }, [loadedStartTimeContainer]);
 
@@ -156,17 +151,111 @@ function DisplayStats() {
     return <></>;
   }
 
-  return <DisplayWriting startTime={moment(loadedStartTimeContainer?.[0].start.toDate())} graphRef = {graphRef} />;
+  return <DisplayWriting startTime={startTime.valueOf()} gaugeRef={gaugeRef} />;
 }
 
+const GaugeMemo = memo(function GaugeMemo({
+  startTime,
+  gaugeRef,
+}: {
+  startTime: number;
+  gaugeRef: any;
+}) {
+  const tickFormatter = useCallback(
+    (v: string) => {
+      return (
+        (Number(v) * 100).toFixed(0) +
+        "%\n" +
+        moment(startTime)
+          .add(Number(v) * 16, "hours")
+          .format("hh:mmA")
+      );
+    },
+    [startTime]
+  );
 
+  const statFormatter = useCallback((percent: Datum | undefined) => {
+    return percent
+      ? `Day Spent: ${(percent["percent"] * 100).toFixed(3)}%`
+      : "0";
+  }, []);
 
+  const config: GaugeConfig = useMemo(
+    (): GaugeConfig => ({
+      percent:
+        moment.duration(moment().diff(moment(startTime))).asSeconds() /
+        (16 * 60 * 60),
+      width: window.innerWidth / 1.25,
+      height: window.innerHeight / 1.25,
+      range: {
+        color: "#30BF78",
+      },
+      renderer: "svg",
+      // innerRadius: 0.7,
+      indicator: {
+        pointer: {
+          style: {
+            lineWidth: 5,
+            stroke: "#D0D0D0",
+          },
+        },
+        pin: {
+          style: {
+            stroke: "#D0D0D0",
+          },
+        },
+      },
+      axis: {
+        nice: true,
+        label: {
+          style: {
+            fill: "black",
+            textBaseline: "middle",
+            fontSize: window.innerWidth > 600 ? window.innerWidth * 0.01 : 10,
+            fontFamily: "'Fira Sans', sans-serif",
+          },
+          offset: window.innerWidth > 600 ? window.innerWidth * -0.035 : -30,
+          formatter: tickFormatter,
+        },
+        tickLine: {
+          length: window.innerWidth > 600 ? window.innerWidth * -0.01 : -20,
+          alignTick: true,
+        },
+        tickInterval: window.innerWidth < 600 ? 0.2 : 0.05,
+        subTickLine: {
+          count: 1,
+        },
+        tickMethod: "time",
+      },
+      statistic: {
+        title: {
+          formatter: statFormatter,
+          style: {
+            color: "black",
+            fontSize: "1rem;",
+          },
+        },
+      },
+      onReady: (plot: any) => {
+        gaugeRef.current = plot;
+      },
+    }),
+    [gaugeRef, startTime, statFormatter, tickFormatter]
+  ); // Percentage-spent intentionally left out since we don't want it to rerender this gauge component everytime. We're just passing it in since we want the gauge to have the correct initial value before the ref loads in an sets the correct value
+  return <Gauge {...config} />;
+});
 
-function DisplayWriting({ startTime, graphRef }: { startTime: Moment, graphRef: any }) {
-
+function DisplayWriting({
+  startTime,
+  gaugeRef,
+}: {
+  startTime: number;
+  gaugeRef: any;
+}) {
+  const startTimeMoment = moment(startTime);
   const timeSpentDuration = moment.duration(moment().diff(startTime));
   const timeLeftDuration = moment.duration(
-    startTime.clone().add(16, "hours").diff(moment())
+    startTimeMoment.clone().add(16, "hours").diff(moment())
   );
 
   const percentageSpent =
@@ -175,112 +264,46 @@ function DisplayWriting({ startTime, graphRef }: { startTime: Moment, graphRef: 
   // const percentageLeft = timeLeftDuration.asSeconds() / (16 * 60 * 60) * 100;
   const percentageLeft = 100 - percentageSpent;
 
-  const config: GaugeConfig = {
-    percent: percentageSpent / 100,
-    width: window.innerWidth / 1.25,
-    height: window.innerHeight / 1.25,
-    range: {
-      color: "#30BF78",
-    },
-    renderer:'svg',
-    // innerRadius: 0.7,
-    indicator: {
-      pointer: {
-        style: {
-          lineWidth: 5,
-          stroke: "#D0D0D0",
-        },
-      },
-      pin: {
-        style: {
-          stroke: "#D0D0D0",
-        },
-      },
-    },
-    axis: {
-      nice: true,
-      label: {
-        style: {
-          fill: "black",
-          // opacity: 0.6,
-          textBaseline:'middle',
-          fontSize: window.innerWidth * 0.01,
-        },
-        offset: -65,
-        formatter: useCallback((v: string) => {
-          return (
-            (Number(v) * 100).toFixed(0) +
-            "%\n" +
-            startTime
-              .clone()
-              .add(Number(v) * 16, "hours")
-              .format("hh:mmA")
-          );
-        }, []),
-      },
-      tickLine:{
-        length: -30,
-        alignTick:true,
-      },
-      tickInterval: window.innerWidth < 600 ? 0.2 : 0.05,
-      subTickLine: {
-        count: 1,
-      },
-      tickMethod: "time",
-    },
-    statistic: {
-      content: {
-        formatter: useCallback((percent: Datum | undefined) => {
-          return percent ? `Day Spent: ${(percent["percent"] * 100).toFixed(3)}%` : "0";
-        }, []),
-
-        style: { 
-          color: "black"
-        },
-      },
-    },
-    onReady: (plot) => {
-      graphRef.current = plot;
-    },
-  };
-  const GaugeMemo = useMemo(() => (<Gauge {...config}/>), []);
   return (
     <>
       <div className="clock">{new Date().toLocaleTimeString()}</div>
       <div className="display-container">
+        <div className="statistics-container">
+          <div className="start-end-time">
+            {startTimeMoment.format("hh:mm A, MMM DD")}
+            <br />
+            {"🡇"}
+            <br />
+            {startTimeMoment.clone().add(16, "hours").format("hh:mm A, MMM DD")}
+          </div>
+          <div className="spent-item">
+            {"Time spent: " +
+              timeSpentDuration.format("h [hrs], m [min], s [secs]", {
+                trim: false,
+              })}
+            <br />
+            {"Time spent in minutes: " +
+              timeSpentDuration.format("m [minutes]", { trim: false })}
+            <br />
+            {"Percentage spent: " + percentageSpent.toFixed(3) + "%"}
+            <br />
+          </div>
+          <div className="left-item">
+            {"Time left: " +
+              timeLeftDuration.format("h [hrs], m [min], s [secs]", {
+                trim: false,
+              })}
+            <br />
+            {"Time left in minutes: " +
+              timeLeftDuration.format("m [minutes]", { trim: false })}
+            <br />
+            {"Percentage left: " + percentageLeft.toFixed(3) + "%"}
+          </div>
+        </div>
         <div className="gauge-container">
-           {GaugeMemo}
-          {/* <DemoGauge/> */}
+          <GaugeMemo gaugeRef={gaugeRef} startTime={startTime} />
         </div>
-        <div className="start-end-times">
-          <div className="start-time-item">
-              <h1 className="start-end-header">Start Time</h1>
-              {startTime.format("hh:mm A, MMM DD")}
-          </div>
-          <div className="end-time-item">
-              <h1 className="start-end-header">End Time</h1>
-              {startTime.clone().add(16, "hours").format("hh:mm A, MMM DD")}
-          </div>
-        </div>
-        {"Time spent: " +
-          timeSpentDuration.format("h [hrs], m [min], s [secs]", {
-            trim: false,
-          })}
-        <br />
-        {"Time spent in minutes: " +
-          timeSpentDuration.format("m [minutes]", { trim: false })}
-        <br />
-        {"Percentage spent: " + percentageSpent.toFixed(3) + "%"}
-        <br />
-        {"Time left: " +
-          timeLeftDuration.format("h [hrs], m [min], s [secs]", {
-            trim: false,
-          })}
-        <br />
-        {"Time left in minutes: " +
-          timeLeftDuration.format("m [minutes]", { trim: false })}
-        <br />
-        {"Percentage left: " + percentageLeft.toFixed(3) + "%"}
+        <div className="conversions-container"></div>
       </div>
     </>
   );
